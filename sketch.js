7b228492950d5af38f39252a91c060ae24c6775b
nour-ghilani <103050470+nour-ghilani@users.noreply.github.com>
2022-04-09 15:37:47 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+var trex ,trex_running;
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  
+
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+  trex = createSprite(55,160,20,50);
+  trex.addAnimation("running",trex_running);
+  ground = createSprite(220,180,400,20);
+  
+}
+
+function draw(){
+  background("white")
+  if(keyDown("space"))
+  {
+   trex.velocityY = -10; 
+  }
+  trex.velocityY = trex.velocityY + 0.5;
+  trex.collide(ground);
+  trex.scale = 0.5
+drawSprites();
+}
